0818fe421f99d818c7aeb4d6c5dd49b437ecf43e
cometsinthesky <lucasferreiraunb@gmail.com>
2024-05-08 02:02:24 -0300

    Add radiation beams Fix checkbox name

--- src/scene-config.js
+++ src/scene-config.js
@@ -105,7 +105,6 @@ for (let i = 0; i < quantidadeParticulas; i++) {
     positions[i * 3 + 2] = z;
 }
 
-
 // Adicionar as posições à geometria das partículas
 particulasGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
@@ -117,6 +116,56 @@ const particulas = new THREE.Points(particulasGeometry, particulasMaterial);
 
 // Adicionar as partículas como filhas da esfera
 pulsar.add(particulas);
+
+
+//ADD PARTÍCULAS CONE NEGATIVO
+// Definir a quantidade de partículas
+const quantidadeParticulas2 = 10000;
+
+// Definir parâmetros do cone
+const raioBaseInicial2 = 0.05;
+const alturaCone2 = 9000;
+const raioBaseFinal2 = 250; // Vértice do cone
+// Criar a geometria das partículas
+const particulas2Geometry = new THREE.BufferGeometry();
+
+// Criar arrays para armazenar as posições das partículas
+const positions2 = new Float32Array(quantidadeParticulas2 * 3);
+
+// Preencher os arrays com posições ao longo do cone
+for (let i = 0; i < quantidadeParticulas2; i++) {
+    // Gerar coordenadas cilíndricas aleatórias dentro do cone
+    const altura = (Math.random() * -1) * alturaCone2;
+    const alturaNormalizada = altura / alturaCone2; // Normaliza a altura entre 0 e 1
+    const theta = Math.random() * (Math.PI * 2) * Math.PI; // Ângulo aleatório
+
+    // Calcula o raio usando uma função cônica para evitar aglomeração nas bordas
+    const raio = raioBaseInicial2 + alturaNormalizada * (raioBaseFinal2 - raioBaseInicial2);
+
+    // Converter coordenadas cilíndricas para cartesianas
+    const x = raio * Math.cos(theta);
+    const z = raio * Math.sin(theta);
+
+    // Definir a posição da partícula
+    positions2[i * 3] = x;
+    positions2[i * 3 + 1] = altura;
+    positions2[i * 3 + 2] = z;
+}
+
+
+// Adicionar as posições à geometria das partículas
+particulas2Geometry.setAttribute('position', new THREE.BufferAttribute(positions2, 3));
+
+// Criar o material das partículas
+const particulas2Material = new THREE.PointsMaterial({ color: 0xfaed3a, size: 0.05 });
+
+// Criar o objeto das partículas
+const particulas2 = new THREE.Points(particulas2Geometry, particulas2Material);
+
+// Adicionar as partículas como filhas da esfera
+pulsar.add(particulas2);
+
+
 
 
 
